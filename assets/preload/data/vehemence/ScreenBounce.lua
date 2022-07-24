10077832d29c1995a55201845c45f73e551de3ee
LuisVasalto <justnameitin@gmail.com>
2022-07-24 00:52:08 -0400

    Vehemence Stuff
    Added elexa gun
    Added vehemence border screens
    
    Things to fix: - The desktop goddammit it is cropped

---
+++ assets/preload/data/vehemence/ScreenBounce.lua
@@ -0,0 +1,27 @@
+local angleshit = 0.2;
+local anglevar = 0.2;
+
+function onBeatHit()
+	if curBeat > 35 and curBeat < 100 or curBeat > 180 and curBeat < 307 or curBeat > 420 then 
+		triggerEvent('Add Camera Zoom', 0.04,0.05)
+
+		if curBeat % 4 == 0 then
+			angleshit = anglevar;
+		else
+			angleshit = -anglevar;
+		end
+		setProperty('camHUD.angle',angleshit*3)
+		setProperty('camGame.angle',angleshit*3)
+		doTweenAngle('turn', 'camHUD', angleshit, stepCrochet*0.002, 'circOut')
+		doTweenX('tuin', 'camHUD', -angleshit*8, crochet*0.001, 'linear')
+		doTweenAngle('tt', 'camGame', angleshit, stepCrochet*0.002, 'circOut')
+		doTweenX('ttrn', 'camGame', -angleshit*8, crochet*0.001, 'linear')
+	else
+		setProperty('camHUD.angle',0)
+		setProperty('camHUD.x',0)
+		setProperty('camHUD.x',0)
+	end
+
+		
+end
+
