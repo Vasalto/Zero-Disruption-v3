2c8d7a66fa1a4e819a9105408d2f8ff2d81a83a1
TheSlithyGamer4evr <williamrunion1@gmail.com>
2022-07-31 13:32:55 -0400

    fix
    there was a problem with me pulling in the commits from github so i had to fix the removed files

---
+++ assets/preload/data/vehemence/script.lua
@@ -0,0 +1,29 @@
+function onCreate()
+    makeLuaSprite('black','',-700,0)
+	makeGraphic('black',2500,2000,'000000')
+    addLuaSprite('black', true)
+    setProperty('black.visible', false)   
+
+    makeLuaSprite('red','',-700,0)
+	makeGraphic('red',2500,2000,'FF0000')
+    addLuaSprite('red', true)
+    setProperty('red.visible', false)   
+end
+
+function onStepHit()
+    if curStep == 144 then
+        setProperty('black.visible', true)
+        doTweenAlpha('byeblack', 'black', 0, 0.7, 'linear');
+    elseif curBeat == 412 then
+        setProperty('red.visible', true)
+        doTweenAlpha('byered', 'red', 0, 0.7, 'linear');
+    end
+end
+
+function onTweenCompleted(tag)
+    if tag == 'byeblack' then
+        removeLuaSprite('black')
+    elseif tag == 'byered' then
+        removeLuaSprite('red')
+    end
+end
