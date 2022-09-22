a70020651dc67e9bcb4d42f5c34d9a214fd0eff9
Small-talking <schreiber.robin@icloud.com>
2022-09-22 15:01:27 +0200

    shtuff
    - WarnState Quickfix (and reformat)
    - deleted some unnecessary files

--- source/WarnState.hx
+++ source/WarnState.hx
@@ -4,24 +4,34 @@ import flixel.text.FlxText;
 import flixel.FlxG;
 import flixel.FlxSprite;
 
-class WarnState extends MusicBeatState {
-override function create()
+class WarnState extends MusicBeatState
 {
-    var menubg:FlxSprite;
+	public static var synced:Bool = false;
 
-menubg = new FlxSprite().loadGraphic(Paths.image('warntext'));
-menubg.antialiasing = ClientPrefs.globalAntialiasing;
-menubg.scrollFactor.set();
-add(menubg);
-super.create();
+	override function create()
+	{
+		var menubg:FlxSprite;
+
+		menubg = new FlxSprite().loadGraphic(Paths.image('warntext'));
+		menubg.antialiasing = ClientPrefs.globalAntialiasing;
+		menubg.scrollFactor.set();
+		add(menubg);
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (synced == true)
+		{
+			FlxG.switchState(new MainMenuState());
+		}
+
+		if (controls.ACCEPT)
+		{
+			synced = true;
+			FlxG.switchState(new MainMenuState());
+			FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
+		}
+		super.update(elapsed);
+	}
 }
-override function update(elapsed:Float)
-{
-if (controls.ACCEPT)
-{
-FlxG.switchState(new MainMenuState());
-FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
-}
-super.update(elapsed);
-}
-} 
